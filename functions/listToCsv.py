732ee5a6626b0c57587d83d9bda8559fdbb2163c
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2021-05-10 15:15:18 +0200

    data nike (csv) : men and women

--- functions/listToCsv.py
+++ functions/listToCsv.py
@@ -14,10 +14,10 @@ def data_list_to_csv(url, brand, gender):
     # print("puma men ") if gender == "men" and brand == "puma" else print("puma women") if gender == "women" and brand == "puma" else print("=== NIKE ===")
     # print("nike men ") if gender == "men" and brand == "nike" else print("nike women") if gender == "women" and brand == "nike" else print("=== PUMA ===")
 
-    # df.to_csv("./csv_files/puma-men.csv") if gender == "men" and brand == "puma" else df.to_csv(
-    #     "./csv_files/puma-women.csv") if gender == "women" and brand == "puma" else df.to_csv(
-    #     "./csv_files/nike-men.csv") if gender == "men" and brand == "nike" else df.to_csv(
-    #     "./csv_files/nike-women.csv") if gender == "women" and brand == "nike" else print("\n===== ERROR =====\n")
+    df.to_csv("./csv_files/puma-men.csv") if gender == "men" and brand == "puma" else df.to_csv(
+        "./csv_files/puma-women.csv") if gender == "women" and brand == "puma" else df.to_csv(
+        "./csv_files/nike-men.csv") if gender == "men" and brand == "nike" else df.to_csv(
+        "./csv_files/nike-women.csv") if gender == "women" and brand == "nike" else print("\n===== ERROR =====\n")
 
     print("puma men") if gender == "men" and brand == "puma" else print(
         "puma women") if gender == "women" and brand == "puma" else print(
